128c7ec43a3dae15f4eba4c1166b265aff829eae
xiongyiwei <717195316@qq.com>
2019-03-27 23:56:17 +0800

    添加样式

--- taskdo/views.py
+++ taskdo/views.py
@@ -11,6 +11,7 @@ import traceback
 class AdhocTask(views.View):
     def get(self, request):
         hostgroup_list = models.HostGroup.objects.all()
+
         return render(request, 'info.html', {'hostgroups': hostgroup_list})
 
     def post(self, request):
